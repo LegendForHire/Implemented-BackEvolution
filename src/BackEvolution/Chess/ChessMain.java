20d547716a0fee957b7cb1a6a0fbe8a470b5bd62
LegendForHire <rhelms@elon.edu>
2018-04-09 15:47:00 -0400

    Chess start

--- src/BackEvolution/Chess/ChessMain.java
+++ src/BackEvolution/Chess/ChessMain.java
@@ -1,5 +1,20 @@
 package BackEvolution.Chess;
 
-public class ChessMain {
+import General.Singleton;
+import General.SpecialMain;
+
+public class ChessMain implements SpecialMain {
+
+	@Override
+	public Singleton SetupStartup() {
+		// TODO Auto-generated method stub
+		return ChessSingleton.getInstance();
+	}
+
+	@Override
+	public void AfterStartup() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/BackEvolution/Chess/ChessNeuron.java
+++ src/BackEvolution/Chess/ChessNeuron.java
@@ -1,7 +1,21 @@
 package BackEvolution.Chess;
 
+import java.util.ArrayList;
+
+import General.Gene;
 import General.Neuron;
 
 public class ChessNeuron extends Neuron {
-
+	public ChessNeuron(){
+		super();
+		//TODO
+	}
+	public ChessNeuron(String method){
+		super(method);
+		//TODO
+	}
+	public ChessNeuron(Neuron n) {
+		super(n);
+		//TODO
+	}
 }
